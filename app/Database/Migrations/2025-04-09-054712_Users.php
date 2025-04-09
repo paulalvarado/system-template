e3658d163dba6eaaa48f79453c83088956942563
paulalvarado <paulperezsv@gmail.com>
2025-04-09 00:07:00 -0600

    first commit

---
+++ app/Database/Migrations/2025-04-09-054712_Users.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+
+class Users extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_user' => [
+                'type'           => 'int',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'firstname' => [
+                'type'       => 'varchar',
+                'constraint' => 150,
+            ],
+            'lastname' => [
+                'type'       => 'varchar',
+                'constraint' => 150,
+            ],
+            'email' => [
+                'type'       => 'varchar',
+                'constraint' => 150,
+            ],
+            'password' => [
+                'type'       => 'varchar',
+                'constraint' => 255,
+            ],
+            'last_login_at' => [
+                'type'       => 'datetime',
+                'default'    => null,
+            ],
+            'status' => [
+                'type'       => 'ENUM',
+                'constraint' => ['active', 'inactive'],
+            ],
+            'created_at' => [
+                'type'       => 'datetime',
+                'default'    => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+            'updated_at' => [
+                'type'       => 'datetime',
+                'default'    => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+        ]);
+        $this->forge->addKey('id_user', true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
---
+++ app/Database/Migrations/2025-04-09-055611_UserMeta.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
+
+class Users extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id_usermeta' => [
+                'type'           => 'int',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'id_user' => [
+                'type'           => 'int',
+                'constraint'     => 11,
+                'not_null'       => true,
+            ],
+            'meta_key' => [
+                'type'       => 'varchar',
+                'constraint' => 150,
+            ],
+            'meta_value' => [
+                'type'       => 'text',
+                'default' => null,
+            ],
+            'created_at' => [
+                'type'       => 'datetime',
+                'default'    => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+            'updated_at' => [
+                'type'       => 'datetime',
+                'default'    => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+        ]);
+        $this->forge->addKey('id_usermeta', true);
+        $this->forge->createTable('usermeta');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('usermeta');
+    }
+}
